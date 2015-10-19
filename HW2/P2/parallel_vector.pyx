18ad93901dccfb039d69e3249d30ce93501a72f5
kevin_c <yufengchen@harvard.edu>
2015-10-19 10:34:19 -0400

    HW2 updates

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -45,6 +45,23 @@ cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
 
     free(<void *> locks)
 
+#helper function to lock pairs in order
+cdef void lock_pair(int i, int j, omp_lock_t *locks) nogil:
+        if i < j:
+            acquire(&locks[i])
+            acquire(&locks[j])
+        elif i>j:
+            acquire(&locks[j])
+            acquire(&locks[i]) 
+
+#helper function to lock pairs in order
+cdef void release_pair(int i, int j, omp_lock_t *locks) nogil:
+        if i < j:
+            release(&locks[i])
+            release(&locks[j])
+        elif i>j:
+            release(&locks[j])
+            release(&locks[i])
 
 ##################################################
 # Your code below
@@ -79,6 +96,7 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
+   '''
    with nogil:
        for r in range(repeat):
            for idx in range(src.shape[0]):
@@ -87,7 +105,38 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                    counts[src[idx]] -= 1
 
    free_N_locks(counts.shape[0], locks)
+   '''
 
+   
+   for r in prange(repeat, nogil=True, num_threads=4):
+       for idx in range(src.shape[0]):
+           if src[idx] != dest[idx]:
+               lock_pair(src[idx], dest[idx], locks)
+               if counts[src[idx]] > 0:
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+               release_pair(src[idx], dest[idx], locks)
+   free_N_locks(counts.shape[0], locks)
+  
+
+   '''
+   for r in prange(repeat, nogil=True, num_threads=4):
+       for idx in range(src.shape[0]):
+
+           # avoid double locking ==> if moving to itself, do nothing
+           if src[idx] != dest[idx]:
+               #with gil:
+               #  print 'prepare locking ... src[idx]=' ,src[idx],'dest[idx]= ', dest[idx], 'idx =', idx
+               lock_pair(src[idx], dest[idx], locks)
+               #with gil:
+               #  print 'locked locks ... src[idx]=' ,src[idx],'dest[idx]= ', dest[idx]
+               if  counts[src[idx]] > 0:
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+               release_pair(src[idx], dest[idx], locks)
+                       
+    free_N_locks(counts.shape[0], locks)
+    '''
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] src,
@@ -105,6 +154,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
+   '''
    with nogil:
        for r in range(repeat):
            for idx in range(src.shape[0]):
@@ -113,3 +163,14 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                    counts[src[idx]] -= 1
 
    free_N_locks(num_locks, locks)
+   '''
+
+   for r in prange(repeat, nogil=True, num_threads=2):
+       for idx in range(src.shape[0]):
+           if src[idx] != dest[idx]:
+               lock_pair(src[idx]/N, dest[idx]/N, locks)
+               if counts[src[idx]] > 0:
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+               release_pair(src[idx]/N, dest[idx]/N, locks)
+   free_N_locks(counts.shape[0], locks)
